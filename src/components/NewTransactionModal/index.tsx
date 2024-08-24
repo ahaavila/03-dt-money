2c3a910601b24e51523b754d0655be49dbd480a1
Augusto Ávila <augusto.avila@ctw.bmwgroup.com>
2024-08-24 22:45:14 +0100

    :feat: add income and outcome buttons on modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { CloseButton, Content, Overlay } from './styles';
-import { X } from 'phosphor-react';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 
 export function NewTransactionModal() {
   return (
@@ -8,11 +8,22 @@ export function NewTransactionModal() {
         <Overlay />
         <Content>
           <Dialog.Title>Nova Transação</Dialog.Title>
-          <CloseButton><X /></CloseButton>
+          <CloseButton><X size={24} /></CloseButton>
           <form action=''>
             <input type="text" placeholder='Descrição' required />
             <input type="number" placeholder='Preço' required />
             <input type="text" placeholder='Categoria' required />
+
+            <TransactionType>
+              <TransactionTypeButton variant='income'>
+                <ArrowCircleUp size={24} />
+                Entrada
+              </TransactionTypeButton>
+              <TransactionTypeButton variant='outcome'>
+                <ArrowCircleDown size={24} />
+                Saída
+              </TransactionTypeButton>
+            </TransactionType>
 
             <button type="submit">Cadastrar</button>
           </form>
